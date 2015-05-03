47329705e3fcb49c96b6274614967f7e4712402c
Matt Nedrich <mattnedrich@gmail.com>
2015-05-03 11:16:21 -0400

    updated mean shift code

--- mean_shift_python.py
+++ mean_shift_python.py
@@ -1,20 +1,21 @@
 
 import math
-import numpy as np
 import sys
-
-def _gaussian_kernel(distance, bandwidth):
-    val = np.exp(-(distance**2 / bandwidth))
-    return val
+import numpy as np
+import point_grouper as pg
+import mean_shift_utils as ms_utils
 
 MIN_DISTANCE = 0.0000001
 class MeanShift(object):
-    def __init__(self, kernel = _gaussian_kernel):
+    def __init__(self, kernel = ms_utils.gaussian_kernel):
         self.kernel = kernel
 
-    def cluster(self, points, kernel_bandwidth):
+    def cluster(self, points, kernel_bandwidth, iteration_callback = None):
+        if(iteration_callback):
+            iteration_callback(points, 0)
         shift_points = np.array(points)
         max_min_dist = 1
+        iteration_number = 1
         while max_min_dist > MIN_DISTANCE:
             print max_min_dist
             max_min_dist = 0
@@ -22,12 +23,16 @@ class MeanShift(object):
                 p_new = shift_points[i]
                 p_new_start = p_new
                 p_new = self._shift_point(p_new, points, kernel_bandwidth)
-                dist = _euclidean_dist(p_new, p_new_start)
+                dist = ms_utils.euclidean_dist(p_new, p_new_start)
                 if(dist > max_min_dist):
                     max_min_dist = dist
                 shift_points[i] = p_new
-
-        return MeanShiftResult(points, shift_points.tolist())
+            if iteration_callback:
+                iteration_callback(shift_points, iteration_number)
+            iteration_number += 1
+        point_grouper = pg.PointGrouper()
+        group_assignments = point_grouper.group_points(shift_points.tolist())
+        return MeanShiftResult(points, shift_points, group_assignments)
 
     def _shift_point(self, point, points, kernel_bandwidth):
         # from http://en.wikipedia.org/wiki/Mean-shift
@@ -41,13 +46,15 @@ class MeanShift(object):
         shifted_point = np.multiply(tiled_weights.transpose(), points).sum(axis=0) / denominator
         return shifted_point
 
-        # #  The above vectorized code is equivalent to the unrolled version below
+        # ***************************************************************************
+        # ** The above vectorized code is equivalent to the unrolled version below **
+        # ***************************************************************************
         # shift_x = float(0)
         # shift_y = float(0)
         # scale_factor = float(0)
         # for p_temp in points:
         #     # numerator
-        #     dist = _euclidean_dist(point, p_temp)
+        #     dist = ms_utils.euclidean_dist(point, p_temp)
         #     weight = self.kernel(dist, kernel_bandwidth)
         #     shift_x += p_temp[0] * weight
         #     shift_y += p_temp[1] * weight
@@ -57,15 +64,8 @@ class MeanShift(object):
         # shift_y = shift_y / scale_factor
         # return [shift_x, shift_y]
 
-def _euclidean_dist(pointA, pointB):
-    if(len(pointA) != len(pointB)):
-        raise Exception("expected point dimensionality to match")
-    total = float(0)
-    for dimension in range(0, len(pointA)):
-        total += (pointA[dimension] - pointB[dimension])**2
-    return math.sqrt(total)
-
 class MeanShiftResult:
-    def __init__(self, original_points, shifted_points):
+    def __init__(self, original_points, shifted_points, cluster_ids):
         self.original_points = original_points
         self.shifted_points = shifted_points
+        self.cluster_ids = cluster_ids
